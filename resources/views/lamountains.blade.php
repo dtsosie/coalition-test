50fc84933755e88bbd0d0755592ad2108691afb5
Daniel Tsosie <dtsosie@@users.noreply.github.com>
2020-07-24 00:59:31 -0500

    Add picture carousel

--- resources/views/lamountains.blade.php
+++ resources/views/lamountains.blade.php
@@ -50,11 +50,25 @@
                 </div>
                 <div id="collapse-one" class="collapse show" data-parent="#nav-tab-content" role="tabpanel" aria-labelledby="heading-one">
                     <div class="card-body">
+
                         <div class="step position-relative">
                             <h1 class="step display-1">01.</h1>
                             <h2 class="step position-absolute">HISTORY</h2>
                         </div>
+
                         <p class="ml-5" style="margin-top: -1em">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in ante viverra, rutrum erat rutrum, consectetur mi. Prion at maximus est. Nullam purus ex, iaculis sed erat sed, blandit tincidunt quam. Cras scelerisque id quam sed digissim Pellentesqu urna nunc, gravida quis henderit ac, tristique ut quam. Vivamus suscipit dignissim tortor nec congue.</p>
+
+                        <div class="carousel slide ml-5" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-25" src="{{ asset('images/lamountains-carousel-1.png') }}">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-25" src="{{ asset('images/lamountains-carousel-2.png') }}">
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
